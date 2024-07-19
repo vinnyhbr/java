856db4019f73410327ad0bb9538c601d53b4d4f9
Vinícius Rosa de Lima <113705995+vinnyhbr@users.noreply.github.com>
2024-07-19 11:53:48 -0300

    Update BancoMain.java

--- src/conta_banco/BancoMain.java
+++ src/conta_banco/BancoMain.java
@@ -4,13 +4,18 @@ public class BancoMain {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner (System.in);
-		String cpf;
-		String nome;
-		System.out.println("Insira seu nome: ");
-		nome = scanner.next();
-		System.out.printf("Ola", nome,"Insira seu CPF: ");
-		cpf = scanner.next();
-		System.out.printf("Conta criada com sucesso");
+		double saldoDaConta = 25;
+		double valorSolicitado = 18;
+		
+		if (valorSolicitado > saldoDaConta){
+			System.out.print("Saldo insuficiente");
+		}
+		else {
+			saldoDaConta -= valorSolicitado	
+		}
+		
+		System.out.printf("Transacao realizada com sucesso! Seu salto agora e " + saldoDaConta);
+		
 	}
 
 }
